1d13f38f0dfe0ad5098be6021c99cb15febda5e7
Ramanujan <joninwien@gmail.com>
2018-10-09 15:54:14 -0700

    Update styles and joke layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './assets/logo.png';
 import './App.css';
 
@@ -14,7 +14,11 @@ class App extends Component {
     console.log('App.js componentWillMount');
 
     this.setState({
-      jokes: zest
+      currentJoke: {
+        setup: 'Click above to see a random joke!'
+      },
+      jokes: zest,
+      jokeCount: 0
     })
   }
 
@@ -23,24 +27,51 @@ class App extends Component {
 
   }
 
+  onButtonClick = e => {
+    // console.log('onButtonClick', e);
+    e.preventDefault();
+
+    const nextJoke = this.setRandomJoke(this.state.jokes);
+
+
+    this.setState({
+      currentJoke: nextJoke,
+      jokeCount: this.state.jokeCount + 1
+    });
+
+    this.fetchImage(nextJoke.punchline);
+  }
+
+  setRandomJoke = obj => {
+    const keys = Object.keys(obj)
+    return obj[keys[ keys.length * Math.random() << 0]];
+  }
+
+  fetchImage = punchline => {
+    console.log('fetchImage punchline', punchline);
+  }
 
   render() {
 
-    console.log('state jokes are', this.state.jokes);
+    console.log('state jokes are', this.state.currentJoke);
+
+    const zest = this.state.currentJoke ? this.state.currentJoke.setup : '.';
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Joke Generator
-          </p>
-
           <button
+            onClick={this.onButtonClick}
             type="button"
             name="testNowBtn"
-            className="App-button">
-            Fetch Joke
+            className="App-button"
+          >
+            Jokes Here {this.state.jokeCount}
           </button>
+
+          <div style={{height: '50px'}}>
+            <p>{zest}</p>
+          </div>
         </header>
       </div>
     );
