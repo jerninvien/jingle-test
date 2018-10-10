e3c5b287d41c6f2ff490a5a2279ae61856c45e0d
Ramanujan <joninwien@gmail.com>
2018-10-10 15:44:07 -0700

    Fix lightbox props silent bug

--- src/components/LightBox/lightbox.js
+++ src/components/LightBox/lightbox.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import { SecondTimer } from 'components/Timer/timer';
 import './lightbox.css';
 
-export const Lightbox = ({ resetCounter }) => {
+export const Lightbox = ({ durationSeconds, resetCounter }) => {
   return(
     <div className='App-lightbox' onClick={resetCounter}>
       <div className='App-opacity-box'></div>
       <p className='App-lightbox-text'>
         Joke Limit Reached. Please wait
-        {<SecondTimer durationSeconds={300} resetCounter={resetCounter} />}
+        {<SecondTimer durationSeconds={durationSeconds} resetCounter={resetCounter} />}
         for the next batch!
       </p>
     </div>
